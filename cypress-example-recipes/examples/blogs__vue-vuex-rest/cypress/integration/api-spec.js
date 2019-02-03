cd2e89eca77642bf782cd60a1f258eaf2e17a87b
Murat Ozcan <muratkerem@gmail.com>
2019-02-03 07:27:50 -0600

    blogs

--- cypress-example-recipes/examples/blogs__vue-vuex-rest/cypress/integration/api-spec.js
+++ cypress-example-recipes/examples/blogs__vue-vuex-rest/cypress/integration/api-spec.js
@@ -1,193 +1,121 @@
+/// <reference types="Cypress" />
 /* eslint-env mocha */
-/* global cy */
-import { enterTodo, getTodoItems, makeTodo, resetDatabase, stubMathRandom, visit } from '../support/utils';
+/* global cy, File */
+import { enterTodo, getNewTodoInput, getTodoApp, getTodoItems, resetDatabase, visit } from '../support/utils';
 
-// testing TodoMVC server API
-describe('via API', () => {
+//
+// very first two tests
+//
+it('loads the app', () => {
+  cy.visit('/')
+  cy.get('.todoapp').should('be.visible')
+})
+
+// enable this test only when the database is empty initially
+// or to demonstrate failure
+it.skip('adds 2 todos', () => {
+  cy.visit('http://localhost:3000')
+  cy.get('.new-todo')
+    .type('learn testing{enter}')
+    .type('be cool{enter}')
+  cy.get('.todo-list li')
+    .should('have.length', 2)
+})
+
+//
+// more realistic tests with resetting data before each test
+//
+describe('UI', () => {
   beforeEach(resetDatabase)
+  beforeEach(() => visit())
 
-  // used to create predictable ids
-  let counter = 1
-  beforeEach(() => {
-    counter = 1
-  })
-
-  const addTodo = title =>
-    cy.request('POST', '/todos', {
-      title,
-      completed: false,
-      id: String(counter++)
+  context('basic features', () => {
+    it('loads application', () => {
+      getTodoApp().should('be.visible')
     })
 
-  const fetchTodos = () => cy.request('/todos').its('body')
+    it('starts with zero items', () => {
+      cy.get('.todo-list').find('li').should('have.length', 0)
+    })
 
-  const deleteTodo = id => cy.request('DELETE', `/todos/${id}`)
+    it('adds two items', () => {
+      enterTodo('first item')
+      enterTodo('second item')
+      getTodoItems().should('have.length', 2)
+    })
 
-  it('adds todo', () => {
-    addTodo('first todo')
-    addTodo('second todo')
-    fetchTodos().should('have.length', 2)
-  })
+    it('enters text in the input', () => {
+      const text = 'do something'
+      getNewTodoInput().type(text)
+      getNewTodoInput().should('have.value', text)
+    })
 
-  it('adds todo deep', () => {
-    addTodo('first todo')
-    addTodo('second todo')
-    fetchTodos().should('deep.equal', [
-      {
-        title: 'first todo',
-        completed: false,
-        id: '1'
-      },
-      {
-        title: 'second todo',
-        completed: false,
-        id: '2'
+    it('can add many items', () => {
+      const N = 100
+      for (let k = 0; k < N; k += 1) {
+        enterTodo(`item ${k + 1}`)
       }
-    ])
-  })
-
-  it('adds and deletes a todo', () => {
-    addTodo('first todo') // id "1"
-    addTodo('second todo') // id "2"
-    deleteTodo('2')
-    fetchTodos().should('deep.equal', [
-      {
-        title: 'first todo',
-        completed: false,
-        id: '1'
-      }
-    ])
-  })
-})
-
-// sets up spying on route "GET /todos" under name "todos"
-// after visiting the page
-// waits for the alias "todos" to make sure
-// the application has actually made the request
-it('loads todos on start', () => {
-  cy.server()
-  cy.route('/todos').as('todos')
-  cy.visit('/')
-  cy.wait('@todos')
-})
-
-// stubs expected API call with JSON response
-// from a fixture file
-it('loads todos from fixture file', () => {
-  cy.server()
-  // loads response from "cypress/fixtures/todos.json"
-  cy.route('/todos', 'fixture:todos')
-  cy.visit('/')
-  getTodoItems()
-    .should('have.length', 2)
-    .contains('li', 'mock second')
-    .find('.toggle')
-    .should('be.checked')
-})
-
-it('initial todos', () => {
-  cy.server()
-  cy.route('/todos', [
-    {
-      title: 'mock first',
-      completed: false,
-      id: '1'
-    },
-    {
-      title: 'mock second',
-      completed: true,
-      id: '2'
-    }
-  ])
-
-  visit(true)
-  getTodoItems()
-    .should('have.length', 2)
-    .contains('li', 'mock second')
-    .find('.toggle')
-    .should('be.checked')
-})
-
-describe('API', () => {
-  beforeEach(resetDatabase)
-  beforeEach(() => visit(true))
-  beforeEach(stubMathRandom)
-
-  it('receives empty list of items', () => {
-    cy.request('todos').its('body').should('deep.equal', [])
-  })
-
-  it('adds two items', () => {
-    const first = makeTodo()
-    const second = makeTodo()
-
-    cy.request('POST', 'todos', first)
-    cy.request('POST', 'todos', second)
-    cy
-      .request('todos')
-      .its('body')
-      .should('have.length', 2)
-      .and('deep.equal', [first, second])
-  })
-
-  it('adds two items and deletes one', () => {
-    const first = makeTodo()
-    const second = makeTodo()
-    cy.request('POST', 'todos', first)
-    cy.request('POST', 'todos', second)
-    cy.request('DELETE', `todos/${first.id}`)
-    cy
-      .request('todos')
-      .its('body')
-      .should('have.length', 1)
-      .and('deep.equal', [second])
-  })
-
-  it('does not delete non-existent item', () => {
-    cy
-      .request({
-        method: 'DELETE',
-        url: 'todos/aaa111bbb',
-        failOnStatusCode: false
-      })
-      .its('status')
-      .should('equal', 404)
-  })
-
-  it('is adding todo item', () => {
-    cy.server()
-    cy
-      .route({
-        method: 'POST',
-        url: '/todos'
-      })
-      .as('postTodo')
-
-    // go through the UI
-    enterTodo('first item') // id "1"
-
-    // thanks to stubbed random id generator
-    // we can "predict" what the TODO object is going to look like
-    cy.wait('@postTodo').its('request.body').should('deep.equal', {
-      title: 'first item',
-      completed: false,
-      id: '1'
+      getTodoItems().should('have.length', N)
     })
   })
 
-  it('is deleting a todo item', () => {
-    cy.server()
-    cy
-      .route({
-        method: 'DELETE',
-        url: '/todos/1'
+  context('advanced', () => {
+    it('adds two and deletes first', () => {
+      enterTodo('first item')
+      enterTodo('second item')
+
+      getTodoItems()
+        .contains('first item')
+        .parent()
+        .find('.destroy')
+        .click({ force: true }) // because it only becomes visible on hover
+
+      cy.contains('first item').should('not.exist')
+      cy.contains('second item').should('exist')
+      getTodoItems().should('have.length', 1)
+    })
+  })
+
+  it('uploads file with todos', () => {
+    let testFile
+
+    // reads test data from JSON file, makes test File object
+    cy.fixture('example.json').then(todos => {
+      const text = JSON.stringify(todos)
+      testFile = new File([text], 'example.json')
+    })
+    // sets test File object on the Vue component
+    cy.window().its('app').then(app => {
+      app.file = testFile
+    })
+    // triggers reading File object
+    cy.get('#todo-file-upload').trigger('change')
+
+    // asserts that items from test JSON file have been
+    // rendered correctly by the component
+    getTodoItems().should('have.length', 4)
+    getTodoItems().eq(1).find('.toggle').should('be.checked')
+  })
+
+  context('cy.tasks', () => {
+    it('can observe records saved in the database', () => {
+      const title = 'create a task'
+      enterTodo(title)
+      // https://on.cypress.io/task
+      cy.task('hasSavedRecord', title, {timeout: 10000})
+    })
+
+    it('returns resolved value', () => {
+      const title = 'create a task'
+      enterTodo(title)
+      // https://on.cypress.io/task
+      cy.task('hasSavedRecord', title, {timeout: 10000})
+      .should('contain', {
+        title,
+        completed: false
       })
-      .as('deleteTodo')
-
-    // go through the UI
-    enterTodo('first item') // id "1"
-    getTodoItems().first().find('.destroy').click({ force: true })
-
-    cy.wait('@deleteTodo')
+      // there is also an ID
+      .and('have.property', 'id')
+    })
   })
 })
