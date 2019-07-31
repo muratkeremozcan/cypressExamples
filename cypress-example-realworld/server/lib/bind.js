3d0b71afff7550c0f788f11287afaa708c967c2d
Murat Ozcan <muratkerem@gmail.com>
2019-07-31 05:36:31 -0500

    remove real world example

--- cypress-example-realworld/server/lib/bind.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const Objection = require('objection');
-
-module.exports = (server) => ({
-    transaction: (fn) => Objection.transaction(server.knex(), fn)
-});
--- cypress-example-realworld/server/lib/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const HauteCouture = require('haute-couture');
-const Package = require('../package.json');
-
-exports.plugin = {
-    pkg: Package,
-    register: HauteCouture.using()
-};
