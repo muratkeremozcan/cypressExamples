3ef55eeeb178d765f1063e39529d03f6c9a6190c
Murat Ozcan <muratkerem@gmail.com>
2020-03-23 14:17:47 -0500

    mailosaur

--- cypress-mailosaur/cypress/integration/mailosaur.spec.js
+++ cypress-mailosaur/cypress/integration/mailosaur.spec.js
@@ -18,25 +18,24 @@ describe('Mailosaur', function () {
 
   it('checks created random email', function () {
     cy.wrap(userEmail).should('exist')
-      .and('include', Cypress.env('MAILOSAUR_USERNAME'));
+      .and('include', Cypress.env('MAILOSAUR_SERVERID'));
   });
 
   // not working yet
-  it.skip('sends email to mailosaur and gets a response', function () {
+  it('sends email to mailosaur and gets a response', function () {
     cy.request({
       method: 'POST',
-      url: `${Cypress.env('MAILOSAUR_API_URL')}/messages/await?server=${Cypress.env('MAILOSAUR_SERVERNAME')}`,
-      body: {
-        sentTo: userEmail,
-        timeout: 10000
-      },
+      url: `${Cypress.env('MAILOSAUR_API')}/messages/await?server=${Cypress.env('MAILOSAUR_SERVERID')}`,
       headers: {
         authorization: Cypress.env('MAILOSAUR_API_KEY')
-      }
+      },
+      body: {
+        sentTo: userEmail,
+      },
     })
-    .should(response => {
-      expect(response.status).to.equal(200);
-      return response.body;
-    });
+    // .should(response => {
+    //   expect(response.status).to.equal(200);
+    //   return response.body;
+    // });
   });
 });
