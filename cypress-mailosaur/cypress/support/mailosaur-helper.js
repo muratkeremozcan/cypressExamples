119e9ed38baa65ef2dcd856bf1c9227b3877b4e2
Murat Ozcan <muratkerem@gmail.com>
2020-03-24 10:19:33 -0500

    retrieve messages

--- cypress-mailosaur/cypress/support/mailosaur-helper.js
+++ cypress-mailosaur/cypress/support/mailosaur-helper.js
@@ -24,3 +24,36 @@ export const postMessageToMailService = query => {
     body: query
   });
 }
+
+/** returns an array of messages */
+export const listMessages = () => {
+  return cy.request({
+    method: 'GET',
+    url: `${Cypress.env('MAILOSAUR_API')}/messages?server=${Cypress.env('MAILOSAUR_SERVERID')}`,
+    headers: {
+      authorization: Cypress.env('MAILOSAUR_PASSWORD')
+    },
+    auth: {
+      user: Cypress.env('MAILOSAUR_API_KEY'),
+      password: ''
+    }
+  }).then(response => {
+    expect(response.status).to.equal(200);
+    return response.body.items; // messages are in the items array
+  });
+}
+
+/** retrieves a message by id */
+export const retrieveMessage = (id) => {
+  return cy.request({
+    method: 'GET',
+    url: `${Cypress.env('MAILOSAUR_API')}/messages/${id}`,
+    headers: {
+      authorization: Cypress.env('MAILOSAUR_PASSWORD')
+    },
+    auth: {
+      user: Cypress.env('MAILOSAUR_API_KEY'),
+      password: ''
+    }
+  });
+}
