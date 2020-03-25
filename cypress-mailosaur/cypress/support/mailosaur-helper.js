05700fcaa769a34ee9ce51194337161a291265db
Murat Ozcan <muratkerem@gmail.com>
2020-03-25 10:56:22 -0500

    refactored

--- cypress-mailosaur/cypress/support/mailosaur-helper.js
+++ cypress-mailosaur/cypress/support/mailosaur-helper.js
@@ -1,3 +1,12 @@
+const commonAuthProps = {
+  user: Cypress.env('MAILOSAUR_API_KEY'),
+  password: ''
+};
+
+const commonHeaders = {
+  authorization: Cypress.env('MAILOSAUR_PASSWORD')
+}
+
 /**
  * Creates a Mailosaur service email
  * @param {string} randomName any random name
@@ -14,52 +23,68 @@ export const postMessageToMailService = query => {
   return cy.request({
     method: 'POST',
     url: `${Cypress.env('MAILOSAUR_API')}/messages/await?server=${Cypress.env('MAILOSAUR_SERVERID')}`,
-    headers: {
-      authorization: Cypress.env('MAILOSAUR_PASSWORD')
-    },
-    auth: {
-      user: Cypress.env('MAILOSAUR_API_KEY'),
-      password: ''
-    },
+    headers: commonHeaders,
+    auth: commonAuthProps,
     body: query,
-    retryOnStatusCodeFailure: true
   });
 }
 
-export const waitForEmailList = () => {
+/** queries Mailosaur and checks if at least 1 email exists in the messages list */
+export const getEmailList = () => {
   return cy.request({
     method: 'GET',
     url: `${Cypress.env('MAILOSAUR_API')}/messages?server=${Cypress.env('MAILOSAUR_SERVERID')}`,
-    headers: {
-      authorization: Cypress.env('MAILOSAUR_PASSWORD')
-    },
-    auth: {
-      user: Cypress.env('MAILOSAUR_API_KEY'),
-      password: ''
-    },
+    headers: commonHeaders,
+    auth: commonAuthProps,
     retryOnStatusCodeFailure: true
   }).its('body').its('items').as('getEmailList') // this is the email list in an array
     .its('length').then(length => length > 0);
 }
 
-export const waitForUserEmail = (userEmail) => {
+/** queries Mailosaur and checks that the specified email exists in the messages list */
+export const getUserEmail = userEmail => {
   return cy.request({
     method: 'GET',
     url: `${Cypress.env('MAILOSAUR_API')}/messages?server=${Cypress.env('MAILOSAUR_SERVERID')}`,
-    headers: {
-      authorization: Cypress.env('MAILOSAUR_PASSWORD')
-    },
-    auth: {
-      user: Cypress.env('MAILOSAUR_API_KEY'),
-      password: ''
-    },
+    headers: commonHeaders,
+    auth: commonAuthProps,
     retryOnStatusCodeFailure: true
   }).its('body').its('items')
-  .then(emailList => emailList[0].to[0].email === userEmail)
-  // .then(emailList => emailList === userEmail);
+    .then(emailList => emailList[0].to[0].email === userEmail)
 }
 
+export const waitUntilUserEmail = userEmail => {
+  const waitUntilOptions = {
+    timeout: 25000,
+    interval: 1000,
+    customMessage: 'wait until an email is received at Mailosaur',
+    errorMsg: 'email did not occur at Mailosaur within time limit',
+    verbose: true,
+    customCheckMessage: 'periodically  checking if email got received'
+  };
 
+  cy.waitUntil(
+    () => getEmailList(),
+    waitUntilOptions
+  );
+
+  // waitUntil expects a boolean result, if the email we need exists, we return true
+  return cy.waitUntil(
+    () => getUserEmail(userEmail),
+    waitUntilOptions
+  );
+}
+
+/** Clean up to reset state. Deletes all messages at https://mailosaur.com/beta/servers/<serverId>/messages*/
+export const deleteAllMessages = () => {
+  return cy.request({
+    method: 'DELETE',
+    url: `${Cypress.env('MAILOSAUR_API')}/messages?server=${Cypress.env('MAILOSAUR_SERVERID')}`,
+    headers: commonHeaders,
+    auth: commonAuthProps,
+    retryOnStatusCodeFailure: true
+  })
+}
 
 // // may be useless, delete if so
 
