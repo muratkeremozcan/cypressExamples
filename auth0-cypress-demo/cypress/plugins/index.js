2ba516ea8f53a87bd278a08019324312f13e601e
Murat K Ozcan <34237651+muratkeremozcan@users.noreply.github.com>
2021-02-12 07:15:54 -0600

    auth0 update

--- auth0-cypress-demo/cypress/plugins/index.js
+++ auth0-cypress-demo/cypress/plugins/index.js
@@ -11,7 +11,15 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const { lighthouse, prepareAudit, pa11y } = require("cypress-audit");
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    lighthouse: lighthouse(),
+    pa11y: pa11y(), 
+  });
+};
--- auth0-cypress-demo/cypress/support/index.js
+++ auth0-cypress-demo/cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import "cypress-audit/commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
